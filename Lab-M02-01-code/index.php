d6a7c396889b5f8eedb75f18d45ce9308ccfeca4
pmundrazebra <96098666+pmundrazebra@users.noreply.github.com>
2021-12-15 10:56:04 +0530

    Add files via upload

--- Lab-M02-01-code/index.php
+++ Lab-M02-01-code/index.php
@@ -13,8 +13,7 @@
 			<section id="banner">
 				<h2>GCP Developer LAB</h2>
 				<! TO DO >
-				<p class="ui header center"><font color="white">Server IP Address <?=
-$_SERVER['SERVER_ADDR'] ?></p>
+<p class="ui header center"><font color="white">Server IP Address <?=$_SERVER['SERVER_ADDR'] ?></p>
 				<ul class="actions">
 					<li><a href="#" class="button special big">Get Started</a></li>
 				</ul>
